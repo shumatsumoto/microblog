48e94ac16f81f29ef9166bc8939f2fa7822c386e
Shu Matsumoto <shumatsumoto0328@gmail.com>
2020-10-24 23:03:40 +0900

    views detail作成

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Blog
 
 
 class BlogListView(ListView):
+	model = Blog
+
+
+class BlogDetalView(DetailView):
 	model = Blog
