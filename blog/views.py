0e203d898662cd630d71809e4b330e76806ccd17
Shu Matsumoto <shumatsumoto0328@gmail.com>
2020-10-25 17:08:51 +0900

    message 続き

--- blog/views.py
+++ blog/views.py
@@ -41,7 +41,19 @@ class BlogUpdateView(UpdateView):
 		url = reverse_lazy('detail', kwargs={'pk': blog_pk})
 		return url
 
+	def form_valid(self, form):
+		messages.success(self.request, '更新しました')
+		return super().form_valid(form)
+
+	def form_invalid(self, form):
+		messages.error(self.request, '更新に失敗しました')
+		return super().form_invalid(form)
+
 
 class BlogDeleteView(DeleteView):
 	model = Blog
 	success_url = reverse_lazy('index')
+
+	def delete(self, request, *args, **kwargs):
+		messages.success(self.request, '削除しました')
+		return super().delete(request, *args, **kwargs)
